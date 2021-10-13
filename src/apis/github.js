d5cad4695c6ec68e1c837c75a5293b8fe2e6beef
deepakbhattmits <deepakbhattmits@gmail.com>
2021-10-13 07:10:05 +0530

    Update github.js

--- src/apis/github.js
+++ src/apis/github.js
@@ -1,10 +1,9 @@
-import axios from 'axios';
-const token = '7c6b06df84bf2071586197be39092e2fb5cd0b39';
+import axios from 'axios'
+const token = '7c6b06df84bf2071586197be39092e2fb5cd0b39'
 export default axios.create({
-    baseURL: `https://api.github.com`,
-    headers: { 
-                contentType: `application/json`,
-                Authorization: `${token}`,
-            }
-});
-
+  baseURL: `https://api.github.com`,
+  headers: {
+    contentType: `application/json`,
+    Authorization: `Bearer ${token}`,
+  },
+})
